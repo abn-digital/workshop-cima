d75bc48dbe3c4c3a0022adbd31e045acdf2850ac
Juan Pablo Brea <juanbrearuiz@gmail.com>
2026-06-08 15:47:40 -0300

    feat: initial commit for CIMA workshop site

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const MIME = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.json': 'application/json'
+};
+
+http.createServer((req, res) => {
+  let filePath = '.' + (req.url === '/' ? '/index.html' : req.url);
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = MIME[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(err.code === 'ENOENT' ? 404 : 500);
+      res.end(err.code === 'ENOENT' ? '404 Not Found' : 'Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+}).listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
